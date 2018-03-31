92c1ff7a426b6e3fadd73cd647b8606be97d4dfe
Brand0nS <brandonscott@students.ptechnyc.org>
2018-03-31 01:39:48 -0400

    Just some Practice with JS
    Practice #1

---
+++ script.js
@@ -0,0 +1,7 @@
+alert ("Please go to the console!");
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name?");
+var Age = prompt("What's your age?");
+ alert ("Nice to meet you, " + firstName + " " + lastName);
+ alert("You are " + Age + " years old.");
+console.log("Also nice to meet you " + firstName + " " + lastName);
